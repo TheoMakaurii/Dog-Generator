9d53fcf7709a49c518f8885ec9edad6151a4fc4d
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-17 14:51:44 -0400

    added index  files outside of webkit

---
+++ index.js
@@ -0,0 +1,31 @@
+// import $ from 'jquery';
+
+function main() {
+  console.log('DOM is loaded');
+  let number = $("#number").val();
+  let url = `https://dog.ceo/api/breeds/image/random/${number}`
+//$('#content-box').append(startMsg);
+
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => { console.log(responseJson)
+        let image= responseJson.message;
+
+        for(let i=0; i<image.length; i++){
+
+    $("#content-box").append(`<img src="${image[i]}" alt = "dog">`);}
+    });
+}
+
+function watchForm() {
+$('form').submit(event => {
+    event.preventDefault();
+    main();
+    });
+}
+
+$(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+  });
+ // $(main);
