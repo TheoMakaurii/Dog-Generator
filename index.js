fb3648d040402f0c19147f0db465486436f0e7ea
Theo McCauley <TheoMakaurii@gmail.com>
2020-10-20 10:13:11 -0400

    style and function updates

--- index.js
+++ index.js
@@ -2,30 +2,30 @@
 
 function main() {
   console.log('DOM is loaded');
-  let number = $("#number").val();
+  let number = $('#number').val();
   let url = `https://dog.ceo/api/breeds/image/random/${number}`
-//$('#content-box').append(startMsg);
+  console.log(url)
 
   fetch(url)
     .then(response => response.json())
-    .then(responseJson => { console.log(responseJson)
-        let image= responseJson.message;
-
-        for(let i=0; i<image.length; i++){
-
-    $("#content-box").append(`<img src="${image[i]}" alt = "dog">`);}
+    .then(responseJson => { console.log(responseJson);
+      let image= responseJson.message;
+      let dogs=``
+      for(let i=0; i<image.length; i++){
+        dogs=image[i];
+$('#dog-house').append(`<img src="${dogs}" alt = "dog">`);}
     });
 }
 
 function watchForm() {
-$('form').submit(event => {
+$('form').on('click', '#submit',() => {
     event.preventDefault();
+    $('#dog-house').html('');
     main();
-    });
+  });
 }
 
 $(function() {
-    console.log('App loaded! Waiting for submit!');
-    watchForm();
-  });
- // $(main);
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
